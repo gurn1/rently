1583a22f3b5db349832fc225520019e99723ffbc
gurn1 <luke@lscwebdesign.co.uk>
2026-06-04 15:12:49 +0100

    chore: select field styling

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -1,10 +1,13 @@
 @props(['options' => [], 'selected' => null, 'placeholder' => null])
 
-<select {{ $attributes->merge(['class' => 'select']) }}>
-    @if ($placeholder)
-        <option value="">{{ $placeholder }}</option>
-    @endif
-    @foreach ($options as $value => $label)
-        <option value="{{ $value }}" @selected($selected == $value)>{{ $label }}</option>
-    @endforeach
-</select>
+<div class="relative">
+    <select {{ $attributes->merge(['class' => 'select']) }}>
+        @if ($placeholder)
+            <option value="">{{ $placeholder }}</option>
+        @endif
+        @foreach ($options as $value => $label)
+            <option value="{{ $value }}" @selected($selected == $value)>{{ $label }}</option>
+        @endforeach
+    </select>
+    <span class="absolute top-[50%] right-4 leading-none translate-y-[-50%] pointer-events-none"><span class="material-symbols-outlined">arrow_drop_down</span></span>
+</div>
